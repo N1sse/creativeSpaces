fd5d25857674a5b2e6b2a561b800ab07cfe06f42
N1sse <ramirezdenisse01@gmail.com>
2025-05-21 17:08:57 -0600

    primer commit

---
+++ creativeSpaces/src/main/java/ac/cr/ucr/creativeSpaces/repository/UserRegister.java
@@ -0,0 +1,59 @@
+package ac.cr.ucr.creativeSpaces.repository;
+
+import ac.cr.ucr.creativeSpaces.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRegister implements IRegisterUser {
+
+    private ArrayList<User> listUser;
+
+    public UserRegister(){
+        this.listUser = new ArrayList<>();
+    }
+
+    @Override
+    public User addUser(User user) {
+       this.listUser.add(user);
+        return user;
+    }//fin addUser
+
+    @Override
+    public List<User> getAllUser() {
+        return this.listUser;
+    }
+
+    @Override
+    public User getUser(Integer id) {
+       for (int i=0; i<this.listUser.size(); i++){
+           if (this.listUser.get(i).getId()==id){
+               return this.listUser.get(i);
+           }
+       }
+        return new User();
+    }
+
+    @Override
+    public User deleteUser(Integer id) {
+        for (int i=0; i<this.listUser.size(); i++){
+            if (this.listUser.get(i).getId()==id){
+                User u=this.listUser.get(i);
+                this.listUser.remove(i);
+                return u;
+            }
+        }
+        return new User();
+    }
+
+    @Override
+    public User editUser(Integer id, User userEdit) {
+        for (int i=0; i<this.listUser.size(); i++){
+            if (this.listUser.get(i).getId()==id){
+                this.listUser.set(i, userEdit);
+                return this.listUser.get(i);
+            }
+        }
+        return new User();
+    }
+}
